32fd207ee951060af7c0d16ef469e5d8b2e4a47d
Pam Willenz <pam@positek.net>
2021-02-18 16:34:24 -0500

    more practice on controller, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'demo#index'
 
   get 'demo/index'
+  get 'demo/hello'
+  get 'demo/about'
+  get 'demo/contact'
 
   #default route
   #get ':controller(/:action(/:id))'
